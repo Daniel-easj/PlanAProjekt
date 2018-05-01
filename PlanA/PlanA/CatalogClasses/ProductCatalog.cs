46cb6cb923adba829f3a7ffa48c3b31f9a81d081
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2018-05-01 12:35:24 +0200

    ProductCatalog implementation start

---
+++ PlanA/PlanA/CatalogClasses/ProductCatalog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlanA.ProductClasses;
+
+namespace PlanA.CatalogClasses
+{
+    class ProductCatalog
+    {
+        private Dictionary<string, IProduct> _productCatalog;
+        private static ProductCatalog _singletonInstance;
+
+        public static ProductCatalog SingletonInstance
+        {
+            get
+            {
+                if (_singletonInstance != null) return _singletonInstance;
+                _singletonInstance = new ProductCatalog();
+                return _singletonInstance;
+            }
+        }
+        public void AddProduct()
+        {
+            
+        }
+
+        public void RemoveProduct(string id)
+        {
+            
+        }
+
+        public void CheckQuantity(IProduct product)
+        {
+            
+        }
+
+        public void ReserveObject(bool yeees)
+        {
+            
+        }
+
+
+    }
+}
