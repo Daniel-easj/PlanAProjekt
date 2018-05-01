31a8d2847e04df808a3f0de1c38c065b6563ea92
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2018-05-01 11:03:24 +0200

    ProductCatalog class created. Some ProductClasses added

---
+++ PlanA/PlanA/ProductClasses/Paint.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanA.ProductClasses
+{
+    class Paint : IProducts, INotifyPropertyChanged
+    {
+        private int _productID;
+        private int _quantity;
+        private bool _isReserved;
+
+        public Paint(int quantity)
+        {
+            _quantity = quantity;
+            _isReserved = false;
+        }
+
+        public int ProductID { get { return _productID; } }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                _quantity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsReserved
+        {
+            get
+            {
+                return _isReserved;
+            }
+            set
+            {
+                _isReserved = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
