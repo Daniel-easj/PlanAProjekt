d163af5668ffe2eb889e67f7781c0e7cf6122587
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2018-05-01 11:21:25 +0200

    IProduct added

---
+++ PlanA/PlanA/ProductClasses/IProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanA.ProductClasses
+{
+    interface IProduct
+    {
+        int ProductID { get; }
+
+        int Quantity
+        {
+            get;
+            set;
+        }
+
+        bool IsReserved
+        {
+            get;
+            set;
+        }
+    }
+}
