577af80cef6a086269900575986d792240304014
TickleMeeeHomo <33862039+TickleMeeeHomo@users.noreply.github.com>
2018-05-03 10:27:00 +0200

    Housing Association Catalog

--- PlanA/PlanA/CatalogClasses/HousingAssociationCustomerCatalog.cs
+++ PlanA/PlanA/CatalogClasses/HousingAssociationCustomerCatalog.cs
@@ -4,10 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PlanA.BaseClasses;
+using PlanA.ProductClasses;
 
 namespace PlanA.CatalogClasses
 {
     class HousingAssociationCustomerCatalog : CatalogBase<HousingAssociationCustomer>
     {
+
+        private Dictionary<int, HousingAssociationCustomer> _housingAssociationCustomer;
+
+
+
+        public HousingAssociationCustomerCatalog()
+        {
+
+            _housingAssociationCustomer = new Dictionary<int, HousingAssociationCustomer>();
+        }
+
+        public Dictionary<int, HousingAssociationCustomer> GetHousingAssociationCustomers;
+
+        public void AddHousingAsscoiationCustomer(int key, HousingAssociationCustomer housingAssociationCustomer)
+
+        {
+
+            _housingAssociationCustomer.Add(key, housingAssociationCustomer);
+
+        }
+
+        public void RemoveHousingAssociationCustomer(int key, HousingAssociationCustomer housingAssociationCustomer)
+        {
+
+
+            _housingAssociationCustomer.Add(key, housingAssociationCustomer);
+
+
+        }
+
     }
+
+
+
 }
