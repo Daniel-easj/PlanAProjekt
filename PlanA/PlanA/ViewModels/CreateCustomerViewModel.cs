6bc116ee234c2bd010fe7431314427fb84d75a5e
MortenMC-easj <31882401+MortenMC-easj@users.noreply.github.com>
2018-05-09 13:50:22 +0200

    Created enum and funktions

--- PlanA/PlanA/ViewModels/CreateCustomerViewModel.cs
+++ PlanA/PlanA/ViewModels/CreateCustomerViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -10,7 +11,63 @@ namespace PlanA.ViewModels
 {
     class CreateCustomerViewModel : INotifyPropertyChanged
     {
+        private bool _cVRenabled;
 
+        public enum CustomerChosen
+        {
+            Privat,
+            Offentlig,
+            Erhverv
+        }
+
+        private CustomerChosen _selectedCustomer;
+
+        public CustomerChosen EnummOrderStatus
+        {
+            get => _selectedCustomer;
+            set
+            {
+                _selectedCustomer = value;
+                SetTextEnabled(value);
+                OnPropertyChanged();
+            }
+        }
+
+        public void SetTextEnabled(CustomerChosen value)
+        {
+            if (value == CustomerChosen.Privat)
+            {
+                _cVRenabled = false;
+            }
+            else if (value == CustomerChosen.Offentlig)
+            {
+                _cVRenabled = false;
+            }
+            else if (value == CustomerChosen.Erhverv)
+            {
+                _cVRenabled = true;
+            }
+        }
+
+        public bool CVRenabled
+        {
+            get => _cVRenabled;
+            set { _cVRenabled = value; OnPropertyChanged(); }
+        }
+
+        public ObservableCollection<CustomerChosen> _CustomerChosens;
+
+        public CreateCustomerViewModel()
+        {
+            _CustomerChosens = new ObservableCollection<CustomerChosen>
+            {
+                CustomerChosen.Privat,
+                CustomerChosen.Offentlig,
+                CustomerChosen.Erhverv
+            };
+        }
+
+        public ObservableCollection<CustomerChosen> CustomerChosens => _CustomerChosens;
 
 
 
