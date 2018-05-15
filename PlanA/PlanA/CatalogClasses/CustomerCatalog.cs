802bdadcbbf08756e0cb37bb2059954a99dac96d
Mart224s <31881991+Mart224s@users.noreply.github.com>
2018-05-15 11:10:22 +0200

    CreatePrivateCustomerCommand changed to CreatCustomerCommand. CreateCustomerCommandViewModel Createt. These Commits are still subjec to change

---
+++ PlanA/PlanA/CatalogClasses/CustomerCatalog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlanA.BaseClasses;
+
+namespace PlanA.CatalogClasses
+{
+    class CustomerCatalog : CatalogBase<CustomerBase>
+    {
+        private CustomerCatalog _singletonInstance;
+
+        private Dictionary<string, CustomerBase> _customerCatalog;
+
+        public CustomerCatalog()
+        {
+            _customerCatalog = new Dictionary<string, CustomerBase>();
+        }
+    }
+}
