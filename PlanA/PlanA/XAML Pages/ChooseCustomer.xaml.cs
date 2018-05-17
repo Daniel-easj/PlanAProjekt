3973ba11a1f40436baf7a99513856d4223eab45a
MortenMC-easj <31882401+MortenMC-easj@users.noreply.github.com>
2018-05-17 11:27:02 +0200

    Made some new pages. 3 of them are blank, but the ChooseCustomer page is almost done. It just needs some bindings

---
+++ PlanA/PlanA/XAML Pages/ChooseCustomer.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PlanA.XAML_Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ChooseCustomer : Page
+    {
+        public ChooseCustomer()
+        {
+            this.InitializeComponent();
+        }
+
+        public void GoToInventoryMainPage(object sender, RoutedEventArgs a)
+        {
+            this.Frame.Navigate(typeof(InventoryMainPage), null);
+        }
+
+        public void GoToCustomerMainPage(object sender, RoutedEventArgs a)
+        {
+            this.Frame.Navigate(typeof(CustomerMainPage), null);
+        }
+
+        public void GoToStatisticPage(object sender, RoutedEventArgs a)
+        {
+            this.Frame.Navigate(typeof(StatisticPage), null);
+        }
+
+        private void GoToMainPage(object sender, RoutedEventArgs a)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+    }
+}
