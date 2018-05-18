d8dd3bee97fd9ab2ff2c1f4feba8b10ba89e4c77
Mart224s <31881991+Mart224s@users.noreply.github.com>
2018-05-18 12:29:17 +0200

    Added the new DataviewModelClasses. ProductDataViewModel is stil empty.

---
+++ PlanA/PlanA/ViewModels/Data/Customer/PrivateCustomerDataViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanA.ViewModels.Data.Customer
+{
+    class PrivateCustomerDataViewModel : DataViewModelAppBase<PrivateCustomer>
+    {
+        public PrivateCustomerDataViewModel(PrivateCustomer obj) : base(obj)
+        {
+        }
+
+        public string Name
+        {
+            get { return DataObject.Name; }
+            set
+            {
+                DataObject.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Phone
+        {
+            get { return DataObject.Phone; }
+            set
+            {
+                DataObject.Phone = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Address
+        {
+            get { return DataObject.Address; }
+            set
+            {
+                DataObject.Address = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get { return DataObject.Email; }
+            set
+            {
+                DataObject.Email = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
