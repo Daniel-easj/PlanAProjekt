b9a0a3c2bb643f0f003f8c56414a1363edd29c62
Mart224s <31881991+Mart224s@users.noreply.github.com>
2018-05-18 12:51:03 +0200

    CoverDataViewModel created and started coding.

---
+++ PlanA/PlanA/ViewModels/Data/Product/CoverDataViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlanA.Data.Domain.ExtendedClasses;
+
+namespace PlanA.ViewModels.Data.Product
+{
+    class CoverDataViewModel : DataViewModelAppBase<PlanAData.Domain.ExtendedClasses.Cover>
+    {
+        public CoverDataViewModel(Cover obj) : base(obj)
+        {
+        }
+
+        public int? Quantity
+        {
+            get { return DataObject.Quantity; }
+            set { DataObject.Quantity = value; }     
+        }
+
+        public string Description
+        {
+            get { return DataObject.Description; }
+            set { DataObject.Description = value; }
+        }
+        
+        public int SpecificType
+        { 
+            get { return DataObject.SpecificType; }
+            set { DataObject.SpecificType = value; }
+
+    }   }
+}
