1d91e8f05b8a6c9dca1ea6a510534e64fa958869
MortenMC-easj <31882401+MortenMC-easj@users.noreply.github.com>
2018-05-28 12:12:40 +0200

    Made a UnitTesting Project and made a class in it to test

---
+++ PlanA/UnitTest/UnitTestingPrivateCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestingPrivateCustomer
+    {
+
+        [TestMethod]
+
+        public void
+
+
+    }
+}
