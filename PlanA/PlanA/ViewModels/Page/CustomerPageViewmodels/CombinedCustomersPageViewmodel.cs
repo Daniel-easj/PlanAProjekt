b1cc08c5377bb60d438afee161a1cfefd2e19dfb
Redspeed93 <31858506+Redspeed93@users.noreply.github.com>
2018-05-28 11:08:23 +0200

    Code cleanup

--- PlanA/PlanA/ViewModels/Page/CustomerPageViewmodels/CombinedCustomersPageViewmodel.cs
+++ PlanA/PlanA/ViewModels/Page/CustomerPageViewmodels/CombinedCustomersPageViewmodel.cs
@@ -1,63 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Data.Transformed.Interfaces;
-using Model.Interfaces;
-using PlanA.BaseClasses;
-using PlanA.Model;
-using PlanA.ViewModels.Base;
-using PlanA.ViewModels.Data.CombinedCustomers;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Data.Transformed.Interfaces;
+//using Model.Interfaces;
+//using PlanA.BaseClasses;
+//using PlanA.Model;
+//using PlanA.ViewModels.Base;
+//using PlanA.ViewModels.Data.CombinedCustomers;
 
-namespace PlanA.ViewModels.Page.CustomerPageViewmodels
-{
-     public class CombinedCustomersPageViewmodel : PageViewModelAppBase<CustomerBase>
-    {
-        public CombinedCustomersPageViewmodel() 
-            : base(DomainModel.Catalogs.CustomersCatalog, new List<string>(), new List<string>())
-        {
-            CombineCustomerCatalogs();
+//namespace PlanA.ViewModels.Page.CustomerPageViewmodels
+//{
+//     public class CombinedCustomersPageViewmodel : PageViewModelAppBase<CustomerBase>
+//    {
+//        public CombinedCustomersPageViewmodel() 
+//            : base(DomainModel.Catalogs.CustomersCatalog, new List<string>(), new List<string>())
+//        {
+//            CombineCustomerCatalogs();
 
-            int count = DomainModel.Catalogs.CustomersCatalog.All.Count;
-            int b = count;
-        }
+//            int count = DomainModel.Catalogs.CustomersCatalog.All.Count;
+//            int b = count;
+//        }
 
-        public void CombineCustomerCatalogs()
-        {
-            foreach (var customer in DomainModel.Catalogs.PrivateCustomerCatalog.All)
-            {
-                if (!DomainModel.Catalogs.CustomersCatalog.IDExists(customer))
-                {
-                    DomainModel.Catalogs.CustomersCatalog.Create(customer);
-                }
+//        public void CombineCustomerCatalogs()
+//        {
+//            foreach (var customer in DomainModel.Catalogs.PrivateCustomerCatalog.All)
+//            {
+//                if (!DomainModel.Catalogs.CustomersCatalog.IDExists(customer))
+//                {
+//                    DomainModel.Catalogs.CustomersCatalog.Create(customer);
+//                }
                 
-            }
+//            }
 
-            foreach (var customer in DomainModel.Catalogs.HousingAssociationCustomerCatalog.All)
-            {
-                if (!DomainModel.Catalogs.CustomersCatalog.IDExists(customer))
-                {
-                    DomainModel.Catalogs.CustomersCatalog.Create(customer);
-                }
+//            foreach (var customer in DomainModel.Catalogs.HousingAssociationCustomerCatalog.All)
+//            {
+//                if (!DomainModel.Catalogs.CustomersCatalog.IDExists(customer))
+//                {
+//                    DomainModel.Catalogs.CustomersCatalog.Create(customer);
+//                }
                 
-            }
+//            }
 
-            foreach (var customer in DomainModel.Catalogs.CompanyCustomersCatalog.All)
-            {
-                if (!DomainModel.Catalogs.CustomersCatalog.IDExists(customer))
-                {
-                    DomainModel.Catalogs.CustomersCatalog.Create(customer);
-                }
-            }
+//            foreach (var customer in DomainModel.Catalogs.CompanyCustomersCatalog.All)
+//            {
+//                if (!DomainModel.Catalogs.CustomersCatalog.IDExists(customer))
+//                {
+//                    DomainModel.Catalogs.CustomersCatalog.Create(customer);
+//                }
+//            }
 
-        }
+//        }
 
 
-        public override IDataWrapper<CustomerBase> CreateDataViewModel(CustomerBase obj)
-        {
-            return new CustomersDataViewmodel(obj);
-        }
-    }
-}
+//        public override IDataWrapper<CustomerBase> CreateDataViewModel(CustomerBase obj)
+//        {
+//            return new CustomersDataViewmodel(obj);
+//        }
+//    }
+//}
