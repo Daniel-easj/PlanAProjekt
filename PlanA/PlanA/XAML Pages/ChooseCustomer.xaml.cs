c40316b1cc12a513956bc75a3a58b2826ad6e874
Morten Marcussen Cramer <mort8653@edu.easj.dk>
2018-05-31 21:05:40 +0200

    Made some Changes

--- PlanA/PlanA/XAML Pages/ChooseCustomer.xaml.cs
+++ PlanA/PlanA/XAML Pages/ChooseCustomer.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using PlanA.XAML_MainPages;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -60,6 +61,11 @@ namespace PlanA.XAML_Pages
         private void GoToNewAssignment(object sender, RoutedEventArgs a)
         {
             this.Frame.Navigate(typeof(NewAssignment), null);
+        }
+
+        private void GoToChooseCustomerMainPage(object sender, RoutedEventArgs a)
+        {
+            this.Frame.Navigate(typeof(ChooseCustomerMainPage), null);
         }
 
 
